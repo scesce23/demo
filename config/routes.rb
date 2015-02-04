25aa7c8fdb8e20a1c456ab791458a4e52be8a575
Scott C. Evans <entertainmentpundit@gmail.com>
2015-02-03 20:12:46 -0800

    add about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Demo::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
-
+  get 'about' => 'site#about'
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
